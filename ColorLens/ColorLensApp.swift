af15f7d27da5ab53f98b25fed00b2ea111f43c75
dimpurr <dimpur@live.com>
2023-08-31 22:57:49 +0100

    xcode init

---
+++ ColorLens/ColorLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLensApp.swift
+//  ColorLens
+//
+//  Created by Cheny Dimpurr on 2023/8/31.
+//
+
+import SwiftUI
+
+@main
+struct ColorLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
